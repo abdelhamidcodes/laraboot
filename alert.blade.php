ffe46c81f29f054292e64a272dca2bc2b04d84d0
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2022-07-04 14:25:55 +0100

    fixed the alert button

--- alert.blade.php
+++ alert.blade.php
@@ -1,7 +1,15 @@
 @props(["dismissible" => false , "type" => "primary"])
 
-$dismissible = $dismissible ? " alert-dismissible fade show" : "";
+@php
+    $dismissibleClass = $dismissible ? " alert-dismissible fade show" : "";
+@endphp
 
-<div {{ $attributes->merge(["role" => "alert", "class" => "alert alert-".$type.$dismissible, ]) }}>
+<div {{ $attributes->merge(["role" => "alert", "class" => "alert alert-".$type.$dismissibleClass, ]) }}>
+    {{-- Content of the alert --}}
     {{ $slot }}
+    
+    {{-- Dismiss button --}}
+    @if($dismissible)
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    @endif
 </div>
